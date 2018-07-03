85c3864826eb075ea31fdc371db027af4c2ec466
Raxz00r <40411266+Raxz00r@users.noreply.github.com>
2018-07-02 21:15:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/guile/pedrotest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.guile.pedrotest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "MyActivity";
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        if (auth.getCurrentUser() != null) {
+//            // already signed in
+//        } else {
+//            startActivityForResult(
+//                    // Get an instance of AuthUI based on the default app
+//                    AuthUI.getInstance().createSignInIntentBuilder().build(),
+//                    RC_SIGN_IN);
+//        }
+
+
+
+
+    }
+
+    protected void logIn(View v) {
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "Cancelaste el logeo", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "Error de conexión a internet", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Log.e(TAG, "Error de logeo: ", response.getError());
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/guile/pedrotest/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.guile.pedrotest;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String phoneNumber = user.getPhoneNumber();
+        Toast.makeText(this, phoneNumber, Toast.LENGTH_LONG).show();
+    }
+
+    protected void logOut(View v) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // user is now signed out
+                        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+    }
+}
+
